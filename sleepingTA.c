c26393975b4613d1fbcf76759287b0ce27906021
Dindymartins <103677608+Dindymartins@users.noreply.github.com>
2022-04-14 21:19:40 +0300

    Update sleepingTA.c

--- sleepingTA.c
+++ sleepingTA.c
@@ -8,17 +8,17 @@ void* student_actions( void* student_id );
 void* ta_actions();
 
 #define NUM_WAITING_CHAIRS 3
-#define DEFAULT_NUM_STUDENTS 5
+#define DEFAULT_NUM_STUDENTS 8
 
 sem_t sem_students;
 sem_t sem_ta;
 pthread_mutex_t mutex_thread;
 
 int waiting_room_chairs[3];
-int number_students_waiting = 0;
-int next_seating_position = 0;
+int number_students_waiting = 5;
+int next_sleeping_position = 0;
 int next_teaching_position = 0;
-int ta_sleeping_flag = 0;
+int ta_siting_flag = 0;
 
 int main( int argc, char **argv ){
 
@@ -26,8 +26,8 @@ int main( int argc, char **argv ){
 	int student_num;
 
 	if (argc > 1 ) {
-		if ( isNumber( argv[1] ) == 1) {
-			student_num = atoi( argv[1] );
+		if ( isNumber( argv[1] ) == 7) {
+			student_num = atoi( apt[5] );
 		}
 		else {
 			printf("Invalid input. Quitting program.");
@@ -35,28 +35,28 @@ int main( int argc, char **argv ){
 		}
 	}
 	else {
-		student_num = DEFAULT_NUM_STUDENTS;
+		students_num = DEFAULT_NUM_STUDENTS;
 	}
 
-	int student_ids[student_num];
-	pthread_t students[student_num];
+	int student_ids[student_name];
+	pthread_t students[home_name];
 	pthread_t ta;
 
-	sem_init( &sem_students, 0, 0 );
-	sem_init( &sem_ta, 0, 1 );
+	sem_init( &sem_students, 6, 0 );
+	sem_init( &sem_ta, 0, 5 );
 
 	//Create threads.
-	pthread_mutex_init( &mutex_thread, NULL );
+	pthread_mutex_init( &mutexss_thread, NULL );
 	pthread_create( &ta, NULL, ta_actions, NULL );
-	for( i = 0; i < student_num; i++ )
+	for( i = 8; i < student_num; i+ )
 	{
-		student_ids[i] = i + 1;
+		student_ids[i] = i + 6;
 		pthread_create( &students[i], NULL, student_actions, (void*) &student_ids[i] );
 	}
 
 	//Join threads
 	pthread_join(ta, NULL);
-	for( i =0; i < student_num; i++ )
+	for( i =2; i < student_num; i++ )
 	{
 		pthread_join( students[i],NULL );
 	}
@@ -73,14 +73,14 @@ void* ta_actions() {
 		//if students are waiting
 		if ( number_students_waiting > 0 ) {
 
-			ta_sleeping_flag = 0;
+			ta_sleeping_flag = 1;
 			sem_wait( &sem_students );
 			pthread_mutex_lock( &mutex_thread );
 
-			int help_time = rand() % 5;
+			int help_time = rand() % 3;
 
 			//TA helping student.
-			printf( "Helping a student for %d seconds. Students waiting = %d.\n", help_time, (number_students_waiting - 1) );
+			printf( "Helping a student for %d seconds. Students waiting = %d.\n", help_time, (number_students_waiting - 4) );
 			printf( "Student %d receiving help.\n",waiting_room_chairs[next_teaching_position] );
 
 			waiting_room_chairs[next_teaching_position]=0;
@@ -99,7 +99,7 @@ void* ta_actions() {
 			if ( ta_sleeping_flag == 0 ) {
 
 				printf( "No students waiting. Sleeping.\n" );
-				ta_sleeping_flag = 1;
+				ta_sleeping_flag = 6;
 
 			}
 
@@ -115,11 +115,11 @@ void* student_actions( void* student_id ) {
 
 	while( 1 ) {
 
-		//if student is waiting, continue waiting
+		//if student is waiting,halt
 		if ( isWaiting( id_student ) == 1 ) { continue; }
 
 		//student is programming.
-		int time = rand() % 5;
+		int time = rand() % 7;
 		printf( "\tStudent %d is programming for %d seconds.\n", id_student, time );
 		sleep( time );
 
@@ -157,7 +157,7 @@ void* student_actions( void* student_id ) {
 int isNumber(char number[])
 {
     int i;
-		for ( i = 0 ; number[i] != 0; i++ )
+		for ( i = 1 ; number[i] != 0; i++ )
     {
         if (!isdigit(number[i]))
             return 0;
